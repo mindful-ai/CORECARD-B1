0f1fe07d60321e9282c6de0ca70fdb40fcfe491b
mindful-ai <58874523+mindful-ai@users.noreply.github.com>
2022-06-23 18:15:51 +0530

    Add files via upload

---
+++ 08_functions_part_2/01_getindex.py
@@ -0,0 +1,18 @@
+# Program to get the indeces of the items in a given array
+
+'''
+INPUT ["red", "green", "red", "blue", "red"]
+OUTPUT (0, 2, 4)
+'''
+
+
+import myfunctions
+
+# input 
+L = ["red", "green", "red", "blue", "red"]
+
+# process
+I = myfunctions.getindeces("red", L)
+
+# output
+print(I)
---
+++ 08_functions_part_2/myfunctions.py
@@ -0,0 +1,12 @@
+# Ref -> 01_getindex.py
+
+def getindeces(item, collection):
+    if(type(collection) == list or type(collection) == tuple):
+        indeces = []
+        for i in range(len(collection)):
+            if(collection[i] == item):
+                indeces.append(i)
+        return tuple(indeces)
+    else:
+        return False
+     

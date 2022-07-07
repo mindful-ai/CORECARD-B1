6e3fcff7f447b8ab7078a1ad260dd6869244a082
mindful-ai <58874523+mindful-ai@users.noreply.github.com>
2022-07-07 17:25:29 +0530

    Add files via upload

---
+++ 18_python_regex_part_3/class_assignment.py
@@ -0,0 +1,17 @@
+students = {'HPE001': {'name': 'Vijay', 'age': '14', 'regid': 'HPE001', 'phy': '99', 'chem': '98', 'math': '97', 'bio': '36', 'avg': '0', 'rank': '0'}, 
+'HPE002': {'name': 'Aryan', 'age': '14', 'regid': 'HPE002', 'phy': '98', 'chem': '61', 'math': '93', 'bio': '96', 'avg': '0', 'rank': '0'}, 
+'HPE003': {'name': 'Muni', 'age': '14', 'regid': 'HPE003', 'phy': '97', 'chem': '98', 'math': '77', 'bio': '94', 'avg': '0', 'rank': '0'}, 
+'HPE004': {'name': 'Abhi', 'age': '14', 'regid': 'HPE004', 'phy': '86', 'chem': '93', 'math': '97', 'bio': '95', 'avg': '0', 'rank': '0'}, 
+'HPE005': {'name': 'Hemanth', 'age': '14', 'regid': 'HPE005', 'phy': '94', 'chem': '91', 'math': '93', 'bio': '96', 'avg': '0', 'rank': '0'}, 
+'HPE006': {'name': 'Grace', 'age': '14', 'regid': 'HPE006', 'phy': '94', 'chem': '65', 'math': '46', 'bio': '67', 'avg': '0', 'rank': '0'}, 
+'CAP007': {'name': 'Ankita', 'age': '14', 'regid': 'CAP007', 'phy': '99', 'chem': '99', 'math': '99', 'bio': '99', 'avg': '0', 'rank': '0'}, 
+'HPE008': {'name': 'John', 'age': '14', 'regid': 'HPE008', 'phy': '96', 'chem': '93', 'math': '77', 'bio': '85', 'avg': '0', 'rank': '0'}, 
+'HPE009': {'name': 'Chris', 'age': '14', 'regid': 'HPE009', 'phy': '54', 'chem': '91', 'math': '43', 'bio': '96', 'avg': '0', 'rank': '0'}, 
+'ORA006': {'name': 'Grace', 'age': '14', 'regid': 'ORA006', 'phy': '67', 'chem': '95', 'math': '96', 'bio': '87', 'avg': '0', 'rank': '0'}, 
+'INF007': {'name': 'Ankita', 'age': '14', 'regid': 'INF007', 'phy': '99', 'chem': '99', 'math': '78', 'bio': '99', 'avg': '0', 'rank': '0'}, 
+'EDG007': {'name': 'Ankita', 'age': '14', 'regid': 'EDG007', 'phy': '99', 'chem': '99', 'math': '78', 'bio': '99', 'avg': '0', 'rank': '0'} }
+
+print(students)
+
+# Calculate the averages for every student in the dictionary above and update the average
+# in the respective dictionary

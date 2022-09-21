621a1bc290164b4fc33c5a694b849aba43b5ca4f
mindful-ai <58874523+mindful-ai@users.noreply.github.com>
2022-09-21 18:50:58 +0530

    Add files via upload

---
+++ 60_sql_part_3/sql_03.sql
@@ -0,0 +1,70 @@
+/* inner join */
+SELECT 
+    first_name,
+    last_name,
+    employees.department_id,
+    departments.department_id,
+    department_name
+FROM
+    employees
+        INNER JOIN
+    departments ON departments.department_id = employees.department_id
+WHERE
+    employees.department_id IN (1 , 2, 3);
+    
+    
+/* left join */
+SELECT
+	c.country_name,
+	c.country_id,
+	l.country_id,
+	l.street_address,
+	l.city
+FROM
+	countries c
+LEFT JOIN locations l ON l.country_id = c.country_id
+WHERE
+	c.country_id IN ('US', 'UK', 'CN');
+    
+/* Grouping Records */
+SELECT
+	department_id,
+	COUNT(employee_id) headcount
+FROM
+	employees
+GROUP BY
+	department_id;
+    
+/* Alter Table */
+
+CREATE TABLE courses (
+    employee_id INT,
+    course_id INT,
+    taken_date DATE,
+    PRIMARY KEY (employee_id , course_id)
+);
+
+ALTER TABLE courses ADD credit_hours INT NOT NULL;
+
+ALTER TABLE courses ADD fee NUMERIC (10, 2) AFTER course_id, ADD course_name INT AFTER taken_date;
+
+ALTER TABLE courses MODIFY fee NUMERIC (10, 2) NOT NULL;
+
+ALTER TABLE courses DROP COLUMN fee;
+
+select * from courses;
+
+/* sub-queries */
+
+SELECT 
+    employee_id, first_name, last_name
+FROM
+    employees
+WHERE
+    department_id IN (SELECT 
+            department_id
+        FROM
+            departments
+        WHERE
+            location_id = 1700)
+ORDER BY first_name , last_name;
